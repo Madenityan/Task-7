bc6ae5e8be22027be18f98903ab9a75cdc80878f
Madenityan <marinakorsyn@gmail.com>
2018-12-13 17:21:17 +0200

    added components on dashboard page

---
+++ src/app/components/circle-chart/circle-chart.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'geek-circle-chart',
+  templateUrl: './circle-chart.component.html',
+  styleUrls: ['./circle-chart.component.scss']
+})
+export class CircleChartComponent implements OnInit {
+
+  constructor() {
+  }
+
+  public chartType = 'polarArea';
+
+  public chartDatasets: Array<any> = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' }
+  ];
+
+  public chartLabels: Array<any> = ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgba(219, 0, 0, 0.1)',
+        'rgba(0, 165, 2, 0.1)',
+        'rgba(255, 195, 15, 0.2)',
+        'rgba(55, 59, 66, 0.1)',
+        'rgba(0, 0, 0, 0.3)'
+      ],
+      hoverBackgroundColor: [
+        'rgba(219, 0, 0, 0.2)',
+        'rgba(0, 165, 2, 0.2)',
+        'rgba(255, 195, 15, 0.3)',
+        'rgba(55, 59, 66, 0.1)',
+        'rgba(0, 0, 0, 0.4)'
+      ],
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+
+  ngOnInit() {
+  }
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+}
