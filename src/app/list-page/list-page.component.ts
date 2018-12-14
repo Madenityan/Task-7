e7a4357bbdc4a56f2a88b95b041b5208b8e1f847
Madenityan <marinakorsyn@gmail.com>
2018-12-14 15:18:30 +0200

    created a showing likes function

--- src/app/list-page/list-page.component.ts
+++ src/app/list-page/list-page.component.ts
@@ -12,43 +12,54 @@ export class ListPageComponent implements OnInit {
       avatar: '../assets/image/user.jpg',
       from: 'Juli Otto',
       subject: 'Lorem ipsum',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      likes: 20
     },
     {
       avatar: '../assets/image/user-2.png',
       from: 'Mark Otto',
       subject: 'Lorem ipsum',
-      content: 'Lorem ipsum dolor'
+      content: 'Lorem ipsum dolor',
+      likes: 24
     },
     {
       avatar: '../assets/image/user.jpg',
       from: 'Jane Octin',
       subject: 'Lorem ipsum',
-      content: 'Consectetur adipiscing elit'
+      content: 'Consectetur adipiscing elit',
+      likes: 60
     },
     {
       avatar: '../assets/image/user-2.png',
       from: 'John Peter',
       subject: 'Lorem ipsum',
-      content: 'In dapibus, turpis ut auctor interdum'
+      content: 'In dapibus, turpis ut auctor interdum',
+      likes: 47
     },
     {
       avatar: '../assets/image/user.jpg',
       from: 'Jane Octin',
       subject: 'Lorem ipsum',
-      content: 'Lorem ipsum dolor sit amet'
+      content: 'Lorem ipsum dolor sit amet',
+      likes: 34
     },
     {
       avatar: '../assets/image/user-2.png',
       from: 'John Peter',
       subject: 'Lorem ipsum',
-      content: 'In dapibus, turpis ut auctor interdum'
+      content: 'In dapibus, turpis ut auctor interdum',
+      likes: 78
     }
   ];
+
+  public show = false;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggle() {
+    this.show = !this.show;
+  }
 }
+
